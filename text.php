e4eb5eb1aa67025b563ee2a57e81b15800436c7d
CarloVeronese <carlo.veronese97@gmail.com>
2023-11-21 13:39:39 +0100

    add - text php file

---
+++ text.php
@@ -0,0 +1,23 @@
+<?php
+
+$text = "What is my perfect crime? I break into Tiffany's at midnight. Do I go for the vault? No, I go for the chandelier. It's priceless. As I'm taking it down, a woman catches me. She tells me to stop. It's her father's business. She's Tiffany. I say no. We make love all night. In the morning, the cops come and I escape in one of their uniforms. I tell her to meet me in Mexico, but I go to Canada. I don't trust her. Besides, I like the cold. Thirty years later, I get a postcard. I have a son and he's the chief of police. This is where the story gets interesting. I tell Tiffany to meet me in Paris by the Trocadero. She's been waiting for me all these years. She's never taken another lover. I don't care. I don't show up. I go to Berlin. That's where I stashed the chandelier.";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Badwords</title>
+</head>
+
+<body>
+
+<p><?php echo $text ?></p>
+<p>The text is <?php echo strlen($text)?> word long</p>
+
+</body>
+
+</html>
